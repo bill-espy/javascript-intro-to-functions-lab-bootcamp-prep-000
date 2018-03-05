8b02b66e0a0f5e4bbf1ca4ff80b42f56bb72a44a
Star Prapakamol <billyaiyai@gmail.com>
2018-03-05 07:47:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ function logWhisper(string){
 
 
 function sayHiToGrandma(string){
-  if (string == string.toUpperCase){ 
+  if (string === "HELLO"){ 
     return "YES INDEED!"
   }
-  else if (string == string.toLowerCase){
+  else if (string === "hello"){
     return "I can\'t hear you!"
   }
   else if (string === "I love you, Grandma."){
