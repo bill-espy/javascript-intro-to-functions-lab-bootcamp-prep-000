ffe9222e4848c8c06a61438288d53d1eaab4c5e8
Star Prapakamol <billyaiyai@gmail.com>
2018-03-05 05:15:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+
+function shout (string){
+  return string.toUpperCase()
+}
+
+function whisper (string){
+return string.toLowerCase() 
+}
+
+
+
+function sayHiToGrandma(string){
+  if (string === "I love you, Grandma."){
+    return "I love you, too."
+  } else if (string = shout){
+    console.log("I can't hear you!")}
+    else if (string = whisper){
+    console.log("YES INDEED!")
+  }
+  
+}
