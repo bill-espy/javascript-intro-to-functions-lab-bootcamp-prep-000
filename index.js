352f7c409a6cce4e33449596f728124283116f7a
Star Prapakamol <billyaiyai@gmail.com>
2018-03-05 07:40:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,10 +33,17 @@ function sayHiToGrandma(string){
   else {}
 }
 
-
-
-
-
+function sayHiToGrandma(string){
+  if (string = string.toUpperCase){ 
+    return "YES INDEED!"
+  }
+  else if (string = string.toLowerCase){
+    return "I can\'t hear you!"
+  }
+  else{
+    return "I love you too"
+  }
+}
 
 
 
